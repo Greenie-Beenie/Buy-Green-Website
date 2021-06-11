dd7a6270d78de909d306bdca78eb3273c30ca995
Leila-U <leila.uy@mail.utoronto.ca>
2021-06-11 16:54:45 -0400

    Create Seasonal Recipe Component for Homepage

---
+++ gb-client/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SeasonalRecipes from '../Components/SeasonalRecipes'
+
+function Home() {
+    return (
+        <SeasonalRecipes/>
+    )
+}
+
+export default Home
