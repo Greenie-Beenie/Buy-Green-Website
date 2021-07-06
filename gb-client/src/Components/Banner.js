f9f1887eadf3a1c1f2bf5a4fe644adb56fddf231
mhasanat <muhammad.hasanat@mail.utoronto.ca>
2021-07-06 01:05:27 -0400

    update to fix banner and nav bar compatiblity

--- gb-client/src/Components/Banner.js
+++ gb-client/src/Components/Banner.js
@@ -10,9 +10,9 @@ import {
 const Banner = () => {
     return (
         <div className = "banner">
-                <EuiTitle   size = 'xxl' className = "banner-text">
+                <EuiTitle style={{ userSelect: "none" }} size = 'xxl' className = "banner-text">
                     <h2>
-                        <EuiTextColor color="ghost">Greenie <br/> Beenie</EuiTextColor>
+                        <EuiTextColor color="ghost">Greenie <br/> Beenie™</EuiTextColor>
                     </h2>
                 </EuiTitle>
         </div>
