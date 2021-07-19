c75a3475abad9f17edd603be6b91a9f510ca83c0
Crawsoir <werq10.fifty@gmail.com>
2021-07-19 15:41:34 -0400

    Replace placeholders for Why Buy Local

--- gb-client/src/data/WhyBuyLocalData.js
+++ gb-client/src/data/WhyBuyLocalData.js
@@ -1,24 +1,19 @@
-import beGreenIcon from '../img/be_green_icon.png';
-import sustainableIcon from '../img/sustainability_icon.png';
-import footprintIcon from '../img/footprint_icon.png';
-import economyIcon from '../img/economy_icon.png';
-
 const avatars = [
     {
-        url: beGreenIcon,
-        name: "Be Green"
+        url: "https://images.unsplash.com/photo-1452948491233-ad8a1ed01085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80",
+        name: "Eat Fresher and Higher Quality"
     },
     {
-        url: sustainableIcon,
-        name: "Sustainability"
+        url: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+        name: "Benefit the Environment"
     },
     {
-        url: footprintIcon,
-        name: "Reduce Carbon Footprint"
+        url: "https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        name: "Create Communities and Connections"
     },
     {
-        url: economyIcon,
-        name: "Support Local Economy"
+        url: "https://images.unsplash.com/photo-1560493676-04071c5f467b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=968&q=80",
+        name: "Support your Local Economy"
     },
 ]
 
